d2588f293c5335b9a3a76093bf0da6689ab7abc2
svetlana ahremchik <pjushampanskoe@inbox.ru>
2024-07-10 17:02:48 +0300

    изменила комментарий приветствия

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import asyncio
 import logging
 import requests
-
+import telebot
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 from config import TOKEN
-
+bot = telebot.TeleBot(token=TOKEN)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
-ASTRO_API_URL = 'https://aztro.sameerkumar.website'
-
+API_URL = "https://aztro.sameerkumar.website"
 
 buttons = [
     KeyboardButton(text="aries"),
@@ -32,7 +31,6 @@ buttons = [
     KeyboardButton(text="pisces")
 ]
 
-# Создание клавиатуры с тремя рядами кнопок
 keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [buttons[0], buttons[1], buttons[2], buttons[3]],
@@ -42,25 +40,37 @@ keyboard = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
+
 @dp.message(Command('start'))
 async def process_start_command(message: types.Message):
-    await message.answer("Выберите ваш знак зодиака:", reply_markup=keyboard)
+    await message.answer("Привет, я бот-гороскоп. Выберите Ваш знак зодиака:", reply_markup=keyboard)
+
 
 def get_horoscope(zodiac_sign):
-    # Пример запроса к API для получения гороскопа (замените данный URL на реальный)
-    url = f'https://example.com/api/horoscope?sign={zodiac_sign}'
-    response = requests.get(url)
-    if response.status_code == 200:
+    try:
+        params = {"sign": zodiac_sign, "day": "today"}
+        response = requests.post(API_URL, params=params)
+
+        if response.status_code != 200:
+            logging.error(f'Error fetching horoscope: {response.status_code} {response.reason}')
+            return "Извините, гороскоп временно недоступен."
+
         data = response.json()
-        return data.get('horoscope', "Гороскоп не найден.")
-    else:
+        horoscope = data.get('description', 'Гороскоп не найден.')
+        logging.info(f'Received horoscope: {horoscope}')
+        return horoscope
+    except Exception as e:
+        logging.error(f'Error fetching horoscope: {e}')
         return "Извините, гороскоп временно недоступен."
+
 
 @dp.message(lambda message: message.text in [button.text for button in buttons])
 async def send_horoscope(message: types.Message):
-    zodiac_sign = message.text
+    zodiac_sign = message.text.lower()
+    logging.info(f'Requested horoscope for: {zodiac_sign}')
     horoscope = get_horoscope(zodiac_sign)
     await message.answer(horoscope)
+
 
 async def main():
     await dp.start_polling(bot)
